6340ba079cca711971daf7c0832e6b3713c3f2f3
nguyenthuan202 <nguyenthuan020010@gmail.com>
2025-01-14 20:19:41 +0700

    initial icon

--- zalo-app/assets/icons/index.jsx
+++ zalo-app/assets/icons/index.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 import Message from './Message'
+import Timeline from './Timeline'
+import Discovery from './Discovery'
+import Profile from './Profile'
+import Setting from './Setting'
+import Search from './Search'
+import QRCode from './QRcode'
+import Add from './Add'
+import ArrowLeft from './ArrowLeft'
+import CallVideoOn from './CallVideoOn'
+import CallVideoOff from './CallVideoOff'
+import MoreHorizontal from './MoreHorizontal'
+import Menu from './Menu'
+import Wallet from './Wallet'
+import Cloud from './Cloud'
 
 
 const icons = {
   message: Message,
+  timeLine: Timeline,
+  discovery : Discovery,
+  profile : Profile,
+  setting : Setting,
+  search : Search,
+  qrCode : QRCode,
+  add : Add,
+  arrowLeft : ArrowLeft,
+  callVideoOn : CallVideoOn,
+  callVideoOff : CallVideoOff,
+  moreHorizontal : MoreHorizontal,
+  menu : Menu,
+  wallet : Wallet,
+  cloud : Cloud,
 }
 
 const Icon = ({name, ...props}) => {

0852ce690ca8b1a68d9f937935ecbdf44f1d249f
Bao44 <tqbao44@gmail.com>
2025-01-14 18:27:06 +0700

    add member on project

--- README.md
+++ README.md
@@ -10,4 +10,4 @@
 | Nguyễn Thanh Thuận|      21080071     |
 | Nguyễn Minh Hải |      21022781        |
 | Lê Minh Thư|      21025781      |
-| Nguyễn Đức Tài |      00000000       |
+| Nguyễn Đức Tài |      21024541        |
--- zalo-app/app/index.jsx
+++ zalo-app/app/index.jsx
@@ -1,6 +1,6 @@
 import { View, Text, Button } from 'react-native'
 import React from 'react'
-import { useRouter } from 'expo-router'
+import { useRouter } from 'expo-router';
 
 const index = () => {
   const router = useRouter();

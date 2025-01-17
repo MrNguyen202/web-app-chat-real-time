453bcf821bb1f32eb3fa091cbb999e4e714f9cbd
Bao44 <tqbao44@gmail.com>
2025-01-17 13:52:52 +0700

    change path

--- zalo-app/app/index.jsx
+++ zalo-app/app/index.jsx
@@ -1,15 +1,12 @@
 import { View, Text, Button } from 'react-native'
 import React from 'react'
 import { useRouter } from 'expo-router';
+import Welcome from './welcome';
 
 const index = () => {
   const router = useRouter();
   return (
-    <View>
-      <Text>index</Text>
-      <Button title='welcome' onPress={() => router.push('welcome')}/>
-      <Text>hello</Text>
-    </View>
+    <Welcome />
   )
 }
 

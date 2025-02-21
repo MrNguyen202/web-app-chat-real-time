cef8a340cb3db7c7b384bbe10ff6a8b6509b6357
Minh Thu <113564040+LMThu28@users.noreply.github.com>
2025-02-21 23:58:33 +0700

    upload redux

--- zalo-web/src/redux/store.js
+++ zalo-web/src/redux/store.js
@@ -1,1 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./userSlice";
+import conversationReducer from "./conversationSlice";
 
+// Tạo store Redux
+export const store = configureStore({
+  reducer: {
+    user: userReducer,               // Quản lý người dùng (login, register)
+    conversation: conversationReducer, // Quản lý các cuộc trò chuyện
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(), // Thêm middleware nếu cần
+  devTools: process.env.NODE_ENV !== 'production', // Kích hoạt devTools trong môi trường phát triển
+});
